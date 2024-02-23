19f7af714a380f9b8226972f536a620e3e473a8a
trinebra <157688079+trinebra@users.noreply.github.com>
2024-02-23 14:13:35 +0100

    update

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ function updateView() {
     `;
 }
 
+
+
 // function PrintNumbers() {
 //     let stopValue = 20;
 //     for (let counter = '2'; counter <= stopValue; counter = counter + 2) {
@@ -19,14 +21,17 @@ function updateView() {
 
 function PrintNumbers() {
     let stopValue = 20
-    document.getElementById("ShowNumbers").innerHTML += '<li>' + 2 + '</li>';
-    document.getElementById("ShowNumbers").innerHTML += '<li>' + 4 + '</li>';
-    document.getElementById("ShowNumbers").innerHTML += '<li>' + 6 + '</li>';
-    document.getElementById("ShowNumbers").innerHTML += '<li>' + 8 + '</li>';
-    document.getElementById("ShowNumbers").innerHTML += '<li>' + 10 + '</li>';
-    document.getElementById("ShowNumbers").innerHTML += '<li>' + 12 + '</li>';
-    document.getElementById("ShowNumbers").innerHTML += '<li>' + 14 + '</li>';
-    document.getElementById("ShowNumbers").innerHTML += '<li>' + 16 + '</li>';
-    document.getElementById("ShowNumbers").innerHTML += '<li>' + 18 + '</li>';
-    document.getElementById("ShowNumbers").innerHTML += '<li>' + 20 + '</li>';
+    for (let counter = 0; counter <= stopValue; counter = counter + 2) {
+        document.getElementById('ShowNumbers').innerHTML += '<li>' + counter + '</li>'
+    }
+    // document.getElementById("ShowNumbers").innerHTML += '<li>' + 2 + '</li>';
+    // document.getElementById("ShowNumbers").innerHTML += '<li>' + 4 + '</li>';
+    // document.getElementById("ShowNumbers").innerHTML += '<li>' + 6 + '</li>';
+    // document.getElementById("ShowNumbers").innerHTML += '<li>' + 8 + '</li>';
+    // document.getElementById("ShowNumbers").innerHTML += '<li>' + 10 + '</li>';
+    // document.getElementById("ShowNumbers").innerHTML += '<li>' + 12 + '</li>';
+    // document.getElementById("ShowNumbers").innerHTML += '<li>' + 14 + '</li>';
+    // document.getElementById("ShowNumbers").innerHTML += '<li>' + 16 + '</li>';
+    // document.getElementById("ShowNumbers").innerHTML += '<li>' + 18 + '</li>';
+    // document.getElementById("ShowNumbers").innerHTML += '<li>' + 20 + '</li>';
 }
